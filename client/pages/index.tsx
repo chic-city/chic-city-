42c92cca43ec525150284ca748b50d60eb70cae0
AbdelkefiMariem <abdelkefimeriem@gmail.com>
2022-12-14 14:46:02 +0100

    nav and footer

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,12 +3,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Navbar from "./navbar"
+import Footer from './footer'
+
 export default function Home() {
   return (
     
     <div >
-  <Link href="/Navbar" > navbar</Link>
-  hello
+      <Navbar/>
+      <Footer/>
+  {/* <Link href="/navbar" > navbar</Link>
+  <Link href="/footer" > footer</Link> */}
+  
     </div>
   )
 }
