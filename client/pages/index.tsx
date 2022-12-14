a0468522650b0bed1bdb5038693b6d7fb578e376
waliid-sliim <walidovicc@gmail.com>
2022-12-14 11:29:15 +0100

    navbar

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-
+import Navbar from "./navbar"
 export default function Home() {
   return (
+    
     <div >
-  
+  <Link href="/Navbar" > navbar</Link>
   hello
     </div>
   )
