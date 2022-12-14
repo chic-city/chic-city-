487f1783492cea75fd0d3f84f208ba05bacfbc7a
Feres Tounsi <113175521+Feres-tounsi@users.noreply.github.com>
2022-12-14 19:01:52 +0100

    Back end done

---
+++ server/models/productSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+var productSchema = mongoose.Schema({
+  articleName: { type: String, require: true },
+  articleType: { type: String, require: true },
+  availableQte: { type: Number, require: true },
+  addedBy: { type: String, require: true },
+  dateOfCreation: { type: String, default: new Date() },
+  description: { type: String, require: true },
+  availableSizes: { type: Array, require: true },
+  availableColor: { type: Array, require: true },
+  type: { type: Array, require: true },
+  category: { type: Array, require: true },
+  price: { type: Number, require: true },
+});
+
+module.exports = mongoose.model("products", productSchema);
