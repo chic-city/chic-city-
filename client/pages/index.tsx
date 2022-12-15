32d7c4614fb48ac645eaf19f4047065dfe8cdbf5
unknown <zayatsunami@gmail.com>
2022-12-15 11:18:43 +0000

    done

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,11 +3,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Navbar from "./navbar"
+import cart from './cart'
 export default function Home() {
   return (
     
     <div >
-  <Link href="/Navbar" > navbar</Link>
+    
+  <Link href="/navbar" > navbar</Link>
+  <Link href="/cart" > cart</Link>
   hello
     </div>
   )
